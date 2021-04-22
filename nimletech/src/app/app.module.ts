1d7e4db352692aa16732272a209a801358604127
norvinnguyen <70989125+norvinnguyen@users.noreply.github.com>
2021-04-22 11:41:24 +0700

    update project structure

--- nimletech/src/app/app.module.ts
+++ nimletech/src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ContactDetailComponent } from './contact-detail/contact-detail.component';
+import { ContactAddComponent } from './contact-add/contact-add.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsComponent,
+    ContactDetailComponent,
+    ContactAddComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
